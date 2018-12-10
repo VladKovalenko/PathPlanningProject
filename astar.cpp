62754608f32f3da53ec0da28cf3383ea55dccd11
VladKovalenko <45144913+VladKovalenko@users.noreply.github.com>
2018-12-10 16:46:06 +0300

    Update astar.cpp

--- astar.cpp
+++ astar.cpp
@@ -9,7 +9,7 @@ Astar::Astar(double HW, bool BT)
 double Astar::computeHFromCellToCell(int i1, int j1, int i2, int j2, const EnvironmentOptions &options)
 {
     if(options.metrictype == CNS_SP_MT_DIAG) {
-        return (abs(abs(i1 - i2) - abs(j1 - j2)) + std::min(abs(i1 - i2), abs(j1 - j2)) * sqrt(2));
+        return (abs(i1 - i2) + abs(j1 - j2)) + std::min(abs(i1 - i2), abs(j1 - j2)) * (sqrt(2) - 1));
     } else if (options.metrictype == CNS_SP_MT_MANH) {
         return (abs(i1 - i2) + abs(j1 - j2));
     } else if (options.metrictype == CNS_SP_MT_EUCL) {
